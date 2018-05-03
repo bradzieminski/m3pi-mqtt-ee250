3aa87e91e37fbb58a94c03364fff4f3fa3bd86cf
Eric Cioffi <ecioffi@usc.edu>
2018-05-03 06:32:37 +0000

    fff

--- ard.py
+++ ard.py
@@ -1,7 +1,12 @@
 import serial
+import re
 
 ser = serial.Serial("/dev/ttyACM0", 19200)
 
 while True:
 	line = ser.readline().decode("utf-8", "replace")
-	print(line)
+	sensors = [int(re.sub('[^0-9]', '', s)) for s in line.split()]
+	if (len(sensors) != 4):
+		continue
+	print(sensors)
+	
